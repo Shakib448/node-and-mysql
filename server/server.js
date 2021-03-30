25dcd5b88fc7a27c54271bea0657cab203fe3418
Shakiba448 <shakiba448@gmail.com>
2021-03-30 12:17:44 +0600

    WIP on NODEJS & MYSQL

--- server/server.js
+++ server/server.js
@@ -27,19 +27,3 @@ app.listen(app.get("PORT"), function () {
       "; press Ctrl-C to terminate."
   );
 });
-// const http = require("http");
-// const ngrok = require("ngrok");
-
-// const port = 80;
-// const server = http.createServer((req, res) => {
-//   res.end("Hello, World!");
-// });
-
-// server.listen(port, (err) => {
-//   if (err) return console.log(`Something bad happened: ${err}`);
-//   console.log(`Node.js server listening on ${port}`);
-
-//   ngrok.connect(port, function (err, url) {
-//     console.log(`Node.js local server is publicly-accessible at ${url}`);
-//   });
-// });
